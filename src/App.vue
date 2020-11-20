34ab7016ebe74f6af4559203b14bbf9ea66df694
Neil McEachin <neil.mceachin@24g.com>
2020-11-19 20:40:37 -0500

    bunch of updates and new tasks added

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <template>
   <div id="app">
     <h1 class="title">Task List</h1>
-    <button @click="printTasksOut()">Print</button>
+    <button @click="copyTasksToClipboard()">Copy Tasks to Clipboard</button>
+    <button @click="showArchived = !showArchived">Show Archived</button>
+    <button @click="showCompleted = !showCompleted">Show Completed</button>
+    <div class="search-tasks">
+      <input type="text" @keypress.enter="searchTasks($event.target.value)" />
+      <button @click="searchTasks()">
+        Search Tasks
+      </button>
+    </div>
     <div class="add-task">
       <input
         type="text"
@@ -12,13 +20,31 @@
         Add Task
       </button>
     </div>
-    <ul>
-      <template v-for="task in tasks">
-        <li :key="task.id" v-if="!task.archived">
+    <select name="" id="" v-model="currentProject">
+      <option :value="false">All</option>
+      <option :value="item" v-for="(item, index) in projects" :key="index">{{
+        item
+      }}</option>
+    </select>
+    <ul :key="searchQuery">
+      <template v-for="task in filteredTasks">
+        <li
+          v-if="
+            (!task.archived || showArchived) &&
+              (!task.completed || showCompleted)
+          "
+          :key="task.id"
+          :class="{
+            isDuplicate: duplicateTasks.includes(task.id),
+            isCompleted: task.completed,
+          }"
+        >
           <Task
             :task="task"
-            @delete="deleteTask(task.id)"
+            @delete="archiveTask(task.id)"
+            @deleteForever="deleteTaskForever(task.id)"
             @checked="checkOffTask(task.id, $event)"
+            @updated="copyTasksToClipboard()"
           />
         </li>
       </template>
@@ -27,7 +53,8 @@
 </template>
 
 <script>
-import tasks from "@/data/newTasks.json";
+import tasks from "@/data/tasks.json";
+import oldTasks from "@/data/tasks.json";
 import Task from "@/components/Task.vue";
 export default {
   name: "App",
@@ -37,48 +64,172 @@ export default {
   data() {
     return {
       taskDescription: null,
+      duplicateTasks: [],
       tasks: tasks,
+      oldTasks: oldTasks,
+      showArchived: false,
+      showCompleted: false,
+      currentProject: false,
+      searchQuery: null,
     };
+  },
+  computed: {
+    tasksLength() {
+      return Object.values(this.tasks).length;
+    },
+    sortedTasks() {
+      let tasks = Object.assign({}, this.tasks);
+      return Object.values(tasks).sort((a, b) => {
+        return new Date(a.createdAt) < new Date(b.createdAt);
+      });
+    },
+    filteredTasks() {
+      let tasks = this.sortedTasks;
+
+      if (this.searchQuery) {
+        return tasks.filter((task) => {
+          return Object.values(task)
+            .toString()
+            .includes(this.searchQuery);
+        });
+      }
+      if (this.currentProject === false) {
+        return tasks;
+      }
+      return tasks.filter((task) => {
+        console.log(JSON.stringify(task).includes(this.searchQuery));
+        if (
+          (task.project && task.project === this.currentProject) ||
+          (this.searchQuery && JSON.stringify(task).includes(this.searchQuery))
+        ) {
+          return true;
+        }
+        return false;
+      });
+    },
+    projects() {
+      let projects = [];
+      this.sortedTasks.forEach((task) => {
+        if (task.project && !projects.includes(task.project)) {
+          projects.push(task.project);
+        }
+      });
+      return projects || null;
+    },
   },
   methods: {
     print(val) {
       console.log(val);
     },
+    updateTaskKeys() {
+      // let oldTasks = Object.assign({}, this.tasks);
+      // this.tasks = {};
+      // Object.values(oldTasks).forEach((task) => {
+      //   this.addTask(task);
+      // });
+    },
+    // importOldTasks() {
+    //   this.oldTasks.forEach((oldTask) => {
+    //     const task = {
+    //       id: Math.random() * 10 + 1,
+    //       createdAt: new Date().toUTCString(),
+    //       description: oldTask.taskName,
+    //       notes: oldTask.notes || null,
+    //       dueBy: null,
+    //       estimatedDuration: null,
+    //       priority: "N",
+    //       reoccurance: null, // Not sure if this should be in task or later schedule
+    //       tags: null,
+    //       sublist: null, // Not sure what do do with this. Maybe it should just be an id for another task?? or I guess an array of id's that make a list or I don't have a sublist but items could have a parent id that I parse and put here?. sublist would be more efficient but either would work.
+    //       completed: oldTask.checked || false,
+    //       archived: false,
+    //     };
+    //     // console.log(oldTask);
+    //     let newTask = {};
+    //     newTask[task.id] = task;
+    //     this.tasks = Object.assign({}, this.tasks, newTask);
+    //   });
+    // },
+    searchTasks(value) {
+      // console.log(value);
+      this.searchQuery = value;
+    },
     addTask() {
       if (this.taskDescription) {
+        let shorthand = this.taskDescription.indexOf("project:");
+        console.log(this.taskDescription[shorthand]);
         const task = {
           id: Math.random() * 10 + 1,
           createdAt: new Date().toUTCString(),
           description: this.taskDescription,
           notes: null,
           dueBy: null,
+          estimatedDuration: null,
           priority: "N",
-          reoccurance: null, //Not sure if this should be in task or later schedule
+          project: this.currentProject || null,
+          reoccurance: null, // Not sure if this should be in task or later schedule
           tags: null,
-          sublist: null, //Not sure what do do with this. Maybe it should just be and id for another task??
+          sublist: null, // Not sure what do do with this. Maybe it should just be an id for another task?? or I guess an array of id's that make a list or I don't have a sublist but items could have a parent id that I parse and put here?. sublist would be more efficient but either would work.
           completed: false,
           archived: false,
         };
-        this.tasks[task.id] = task;
+        let newTask = {};
+        newTask[task.id] = task;
+        // Due to limiation with reactivity when dealing with objects or keys that havent been defined in data() new objects need to be defined like so. https://vuejs.org/v2/guide/reactivity.html#Change-Detection-Caveats for more details
+        this.tasks = Object.assign({}, this.tasks, newTask);
         this.taskDescription = null;
+        this.copyTasksToClipboard();
       }
     },
     checkOffTask(id, val) {
-      console.log(id, val);
-      this.$set(this.tasks, id, val);
-      // this.tasks[id].completed = val;
+      // console.log(id, val);
+      this.tasks[id].completed = val;
+      this.$forceUpdate();
     },
-    deleteTask(id) {
+    archiveTask(id) {
       this.tasks[id].archived = true;
     },
-    sortTasks() {
-      // Object.values(this.tasks).sort((a, b) => {
-      //   new Date(a.createdAt) > new Date(b.createdAt);
-      // });
+    deleteTaskForever(id) {
+      delete this.tasks[id];
+      const updatedTasks = this.tasks;
+      updatedTasks[id] = null;
+      this.tasks = updatedTasks;
+      // Need to force update. I don't know if this appropriate or if I just need to be doing something different
+      this.$forceUpdate();
     },
-    printTasksOut() {
-      console.log(JSON.stringify(this.tasks, null, 2));
+    findDuplicates() {
+      this.duplicateTasks = [];
+      Object.values(this.tasks).forEach((compare) => {
+        Object.values(this.tasks).forEach((task) => {
+          if (
+            compare.description === task.description &&
+            compare.id !== task.id &&
+            !this.duplicateTasks.includes(compare.id)
+          ) {
+            this.duplicateTasks.push(task.id);
+          }
+        });
+      });
+      return this.duplicateTasks;
     },
+    copyTasksToClipboard() {
+      const tasks = JSON.stringify(this.tasks, null, 2);
+      navigator.clipboard.writeText(tasks).then(
+        function() {
+          console.log("Async: Copying to clipboard was successful!");
+        },
+        function(err) {
+          console.error("Async: Could not copy text: ", err);
+        }
+      );
+    },
+  },
+  mounted() {
+    // if (this.oldTasks) {
+    // this.importOldTasks(); // Use cautiously as it will create duplicates
+    // }
+    // this.updateTaskKeys();
+    // this.findDuplicates();
   },
 };
 </script>
@@ -91,22 +242,54 @@ export default {
   color: #2c3e50;
   margin: 0 auto;
   margin-top: 60px;
-  max-width: 800px;
+  // max-width: 800px;
 }
 ul {
   padding: 0;
+  display: flex;
+  flex-wrap: wrap;
+  box-sizing: border-box;
   // margin: 0;
+  * + * {
+  }
   li {
     list-style-type: none;
-    margin: 0;
-    margin-bottom: 10px;
+    // margin: 0;
+    // margin-bottom: 10px;
+    margin-left: 20px;
+    margin-top: 20px;
+    padding: 10px 20px;
+    border: 2px dashed black;
+    box-sizing: border-box;
+    width: 48%;
+
+    &.isDuplicate {
+      border-color: red;
+    }
+    &.isCompleted {
+      border-color: rgb(28, 80, 28);
+      background-color: rgb(148, 236, 148);
+    }
   }
 }
 .title {
   text-align: center;
 }
-.add-task {
+.add-task,
+.search-tasks {
   display: flex;
   justify-content: center;
+  max-width: 500px;
+
+  input {
+    flex-grow: 1;
+  }
+}
+.add-task {
+  margin: 0 auto;
+  margin-top: 20px;
+}
+.search-tasks {
+  display: inline-flex;
 }
 </style>
--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,22 +1,79 @@
 <template>
   <div
     class="task"
-    :class="{ checked: task.completed }"
-    @click="taskClicked()"
+    :class="{ isCompleted: task.completed, isArchived: task.archived }"
   >
     <input
       type="checkbox"
-      :value="checked"
-      @change="$emit('checked', checked)"
+      v-model="checked"
+      @change="$emit('checked', checked) && $emit('updated')"
     />
     <div class="wrapper">
-      <div class="name">{{ task.description }}</div>
-      <div v-if="task.notes && expand" class="small notes">
-        {{ task.notes }}
+      <div class="name">
+        <textarea
+          @input="$emit('updated')"
+          v-model="task.description"
+          type="text"
+        />
       </div>
-      <hr v-if="task.notes && !expand" />
+      <div class="extra-info" v-if="!compact">
+        <div class="estimatedDuration inputWrapper">
+          Estimated Duration:
+          <input
+            @input="$emit('updated')"
+            v-model="task.estimatedDuration"
+            type="text"
+          />
+        </div>
+        <div class="project inputWrapper">
+          Priority :
+          <select @change="$emit('updated')" v-model="task.priority">
+            <option value="L">L</option>
+            <option value="N">N</option>
+            <option value="H">H</option>
+          </select>
+        </div>
+        <div class="tags infoWrapper">
+          Tags:
+          <span class="tag" v-for="(item, index) in tags" :key="index">
+            {{ item }}
+          </span>
+        </div>
+        <div class="inputWrapper">
+          Tag Input (CSV):
+          <input @input="$emit('updated')" v-model="task.tags" type="text" />
+        </div>
+        <div class="project inputWrapper">
+          Project :
+          <input @input="$emit('updated')" v-model="task.project" type="text" />
+        </div>
+        <div class="small notes inputWrapper">
+          notes:
+          <textarea v-model="task.notes" @input="$emit('updated')"></textarea>
+        </div>
+        <div class="project inputWrapper">
+          startDate :
+          <input
+            @input="$emit('updated')"
+            v-model="task.startDate"
+            type="text"
+          />
+        </div>
+        <div class="project inputWrapper">
+          dueDate :
+          <input @input="$emit('updated')" v-model="task.dueBy" type="text" />
+        </div>
+        <div class="infoWrapper details small">
+          createdAt: {{ formattedDate(task.createdAt) }}
+        </div>
+      </div>
     </div>
-    <button @click="$emit('delete')">Delete</button>
+    <button v-if="!task.archived" @click="$emit('delete') && $emit('updated')">
+      Delete
+    </button>
+    <button v-else @click="$emit('deleteForever') && $emit('updated')">
+      Delete Forever
+    </button>
   </div>
 </template>
 
@@ -25,50 +82,99 @@ export default {
   props: {
     task: {
       type: Object,
-      default: null,
+      default: () => {},
     },
+    compact: Boolean,
   },
   data() {
     return {
       checked: false,
-      expand: false,
+      notes: null,
     };
   },
-  methods: {
-    taskClicked() {
-      this.$emit("click");
-      this.expand = !this.expand;
+  computed: {
+    tags() {
+      if (!this.task.tags) return null;
+      return this.task.tags.split(",");
     },
   },
   mounted() {
     if (this.task.completed) {
       this.checked = this.task.completed;
     }
+    if (!this.task.estimatedDuration) {
+      this.task.estimatedDuration = 0;
+    }
+    if (this.task.notes) {
+      this.notes = this.task.notes;
+    }
+  },
+  methods: {
+    formattedDate(time) {
+      let current_datetime = new Date(time);
+      let formatted_date =
+        current_datetime.getMonth() +
+        1 +
+        "-" +
+        current_datetime.getDate() +
+        "-" +
+        current_datetime.getFullYear() +
+        " " +
+        current_datetime.getHours() +
+        ":" +
+        current_datetime.getMinutes() +
+        ":" +
+        current_datetime.getSeconds();
+      return formatted_date;
+    },
   },
 };
 </script>
 
 <style lang="scss" scoped>
+textarea {
+  width: 100%;
+  padding: 20px;
+  box-sizing: border-box;
+}
 .task {
-  padding: 10px 20px;
-  border: 2px dashed black;
   cursor: pointer;
   display: flex;
   align-items: flex-start;
 
-  &.checked {
+  &.isCompleted {
     .name {
       text-decoration: line-through;
     }
   }
+  &.isArchived {
+    opacity: 0.5;
+  }
 
   .wrapper {
     padding: 0 20px;
+    flex-grow: 1;
+  }
+
+  .inputWrapper,
+  .infoWrapper {
+    margin-top: 20px;
+  }
+
+  .tags {
+    display: flex;
+    align-items: center;
+    flex-wrap: wrap;
+    .tag {
+      padding: 5px;
+      margin: 5px;
+      background-color: aqua;
+      border-radius: 3px;
+    }
   }
 
   .notes {
-    margin-top: 10px;
-    margin-left: 10px;
+    // margin-left: 10px;
   }
   button {
     margin-left: auto;
